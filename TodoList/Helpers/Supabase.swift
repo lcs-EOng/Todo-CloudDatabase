53ca1e59d55b80d3b7acbf14ea2fab87f46a3616
Eason Ong <eong@lcs.on.ca>
2024-05-22 12:07:20 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,8 @@
+import Supabase
+import Foundation
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://eqdjntkvzqpxnsmpgyal.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZGpudGt2enFweG5zbXBneWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0NjQ3NzYsImV4cCI6MjAyOTA0MDc3Nn0.bsrfzEW1EfDS22FsxcGCHKuE_VwDHNjeFMGo6xjysr0"
+)
+
